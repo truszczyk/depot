ace78fd5f837676e209fc8f2d37d5b5069e13a44
h.pio <h.pio@o2.pl>
2013-12-19 21:18:19 +0100

    rozdzial8

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,20 @@
 class StoreController < ApplicationController
 	def index
 		@products = Product.order(:title)
+		@time = Time.now
 	end
+end
+
+
+
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+	get :index
+	assert_response :success
+	assert_select '#columns #side a', minimum: 4  
+	assert_select '#main .entry', 3  
+	assert_select 'h3', 'Programming Ruby 1.9'  
+	assert_select '.price', /\$[,\d]+\.\d\d/  
+  end
 end
